9d5ae70af8685140d7d5ae010955a8923218ce39
ZoneESX <121647040+ZoneESX@users.noreply.github.com>
2022-12-30 01:27:13 +0100

    Create sv.lua

---
+++ [esx_addons]/esx_barbershop/locales/sv.lua
@@ -0,0 +1,9 @@
+Locales['sv'] = {
+  ['valid_purchase'] = "Validera det här köpet?",
+  ['yes'] = "ja",
+  ['no'] = 'nej',
+  ['not_enough_money'] = "du har inte tillräckligt med pengar",
+  ['press_access'] = "Tryck på [E] för att komma åt frisörsalongen.",
+  ['barber_blip'] = 'Barber Shop',
+  ['you_paid'] = 'du betalade SEK%s',
+}
